e364d89b81b921f51c9e3675af235c3dfa2cccdf
Marika <marika@DESKTOP-SGB6HND.localdomain>
2022-10-19 14:27:03 +0100

    changes to api gateway and lambda code

--- lambda/dynamo_increment.js
+++ lambda/dynamo_increment.js
@@ -1,21 +1,24 @@
-const AWS = require("aws-sdk"); // is this needed here?
+const AWS = require("aws-sdk");
 
-const dynamo = new AWS.DynamoDB.DocumentClient(); // is this needed here?
+const dynamo = new AWS.DynamoDB.DocumentClient();
 
 
-exports.handler = async (event, context) => {
+exports.handler = async () => {
 
   let responseBody = "";
   let statusCode = 0;
   let headers = '';
 
   const params = {
-    TableName: 'visitorCount',
+    TableName: 'visitorCounter',
     Key: {
       count: 'totalVisitors'
     },
-    UpdateExpression: "SET numberOfVisitors = numberOfVisitors + :inc",
-    ExpressionAttributeValues: { ":inc": 1 },
+    UpdateExpression: "SET numberOfVisitors = if_not_exists(numberOfVisitors, :initial) + :inc",
+    ExpressionAttributeValues: {
+      ":initial": 0,
+      ":inc": 1,
+    },
     ReturnValues: "UPDATED_NEW"
   };
 
@@ -26,7 +29,7 @@ exports.handler = async (event, context) => {
     statusCode = 200;
     headers = {
       "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Origin": "https://d2000v0om9ep85.cloudfront.net",
+      "Access-Control-Allow-Origin": "https://marikabergman.com",
       "Access-Control-Allow-Methods": "OPTIONS,POST,GET,"
     };
     responseBody = JSON.stringify(data);
--- lib/portfolio_backend-stack.ts
+++ lib/portfolio_backend-stack.ts
@@ -2,24 +2,14 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { LambdaToDynamoDBProps, LambdaToDynamoDB} from '@aws-solutions-constructs/aws-lambda-dynamodb';
-import * as apigw from 'aws-cdk-lib/aws-apigateway';  //this is rest api gw????
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'; //? is this needed?
-//import * as apigw from 'aws-cdk-lib/aws-apigatewayv2';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';  
 import { AttributeType } from 'aws-cdk-lib/aws-dynamodb';
-import { Cors } from 'aws-cdk-lib/aws-apigateway';
 
 export class PortfolioBackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-
-// https://docs.aws.amazon.com/solutions/latest/constructs/walkthrough-part-2-v2.html
-    const addVisitor = new lambda.Function(this, 'addVisitorHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-        code: lambda.Code.fromAsset('lambda'),
-        handler: 'dynamo_increment.handler',
-    });
-
+    
     // defines an AWS Lambda resource
     const lambdaToDynamo: LambdaToDynamoDBProps = {
       lambdaFunctionProps: { // User provided props to override the default props for the Lambda function
@@ -27,37 +17,39 @@ export class PortfolioBackendStack extends Stack {
         code: lambda.Code.fromAsset('lambda'),
         handler: 'dynamo_increment.handler',
       },
-      tablePermissions: 'ReadWrite', // Lambda allowed to read and write
+
       dynamoTableProps: {
-        tableName: 'visitorCount', // forcing tableName to be visitorCount to match the lambda function
+        tableName: 'visitorCounter', // forcing tableName to be visitorCount to match the lambda function
         partitionKey: { 
           name: 'count',
           type: AttributeType.STRING
         },
 
-      } //Optional user provided props to override the default props for DynamoDB Table
-      // default option is to the the billing mode for DynamoDB to on-demand
+      } 
     };
 
-    new LambdaToDynamoDB(this, 'visitorCounter', lambdaToDynamo);
-
+    const lambdaToDyn = new LambdaToDynamoDB(this, 'visitorCounter', lambdaToDynamo);
   
+
+    
     const api = new apigw.LambdaRestApi(this, 'portfolio_api', {
-      handler: addVisitor,
+      handler: lambdaToDyn.lambdaFunction,
       proxy: false,
-     //? integrationOptions: i guess nothing needed here?
      defaultCorsPreflightOptions: {
-      allowOrigins: Cors.ALL_ORIGINS, //! change here 
-     }
+      allowHeaders:[
+        'Content-Type'
+      ],
+      allowMethods: ['OPTIONS', 'POST'],
+      allowOrigins: ['https://marikabergman.com'],
+     },
     })
 
-    // methods that are used
-    api.root.addMethod('POST'); //? is options needed?
+  
+    api.root.addMethod('POST'); 
 
-    //! should I add stages?
     
     api.addUsagePlan('UsagePlan', {
-      throttle: {
+        throttle: {
         rateLimit: 50,
         burstLimit: 20
       }
