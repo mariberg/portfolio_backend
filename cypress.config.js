98cc8d38ca55af71b0ac8b76db862bcae8df0d42
Marika <marika@DESKTOP-SGB6HND.localdomain>
2022-10-27 08:38:03 +0100

    correction to gitignore

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+    baseUrl: "https://x9yxbl5ntc.execute-api.eu-west-2.amazonaws.com/prod/",
+  },
+});
---
+++ cypress/e2e/visitorCount_test.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+
+describe('Returns an updated value from database', () => {
+  it('fetches visitorCount', () => {
+    cy.request('POST', '/')
+      .then((resp) => {
+        const data = JSON.parse(resp.body);
+
+        expect(resp.status).to.eq(200)
+
+        expect(data.Attributes.numberOfVisitors).to.not.be.oneOf([null, "", undefined])
+      })
+  })
+})
