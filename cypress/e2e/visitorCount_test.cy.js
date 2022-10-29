ecc7567472390f60f1d568cb2e977b4197eed597
Marika <marika@DESKTOP-SGB6HND.localdomain>
2022-10-29 16:40:47 +0100

    correction to cypress test

--- cypress/e2e/visitorCount_test.cy.js
+++ cypress/e2e/visitorCount_test.cy.js
@@ -5,7 +5,7 @@ describe('Returns an updated value from database', () => {
   it('fetches visitorCount', () => {
     cy.request('POST', '/')
       .then((resp) => {
-        const data = JSON.parse(resp.body);
+        const data = resp.body;
 
         expect(resp.status).to.eq(200)
 
